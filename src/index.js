ad4b7b11239c5471d1bce05e8b91391ee29f75fc
Mariana Arenas <arenasm8@hotmail.com>
2022-11-15 18:09:27 -0300

    funciones listas, impresion resultado en html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//capturar valor del input de nombre 
+
+let playerName = document.getElementById("playerName").value;
+// llamar funciones
+
+//crear eventos click para cifrar y descifrar
+
+let cifrar = document.getElementById("cipher"); //id cipher es el botón
+
+cifrar.addEventListener("click", function(){ //acá tengo que nombrar la función para llamarla
+
+    let displacement = document.getElementById("displacement").value;
+    let originalText = document.getElementById("originalText").value;
+    let mensajeCifrado = cipher.encode(displacement, originalText)
+    //capturar valor texto original y número de desplazamientos
+    document.getElementById("textoFinal").innerHTML = mensajeCifrado;
+   });
+
+   
+
+let decipher = document.getElementById("decipher"); //boton que llama funcion descifrar
+ decipher.addEventListener("click",function(){
+    let displacement = document.getElementById("displacement").value; 
+    let originalText = document.getElementById("originalText").value;
+    let mensajeDescifrado = cipher.decode(displacement, originalText)
+    document.getElementById("textoFinal").value = mensajeDescifrado;
+});
+
+    
+ 
+
+// me falta que guarde el valor del resultado de la función en alguna parte, para poder mostrarlo 
+
+
+
+
+
